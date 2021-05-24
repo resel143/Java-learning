5a46140874c833803242d600b60748a1a6be8e98
Reshul <53306036+resel143@users.noreply.github.com>
2021-05-24 10:56:32 +0530

    Create arrayobjclass.java

---
+++ arrayobjclass.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        jagged array
+        int roll[][] = new int[3][];
+        Class c = roll.getClass();
+        String name = c.getName();
+        System.out.print(name);
+
+    }
+
+}
